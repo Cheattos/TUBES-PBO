fae0cafa25f841ed4b1d938d3dfa03c491878095
Heridho118140173 <65078571+heridho118140173@users.noreply.github.com>
2020-05-15 20:36:15 +0700

    Update db_bengkel.sql
    Penambahan table tb_mobil, tb_montir dan pengisian data tabel awal

--- db_bengkel.sql
+++ db_bengkel.sql
@@ -51,3 +51,43 @@ CREATE TABLE `tb_konsumen` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+--
+-- Table structure for table `tb_mobil`
+--
+
+CREATE TABLE `tb_mobil` (
+  `kode_mobil` varchar(7) DEFAULT NULL,
+  `nama_mobil` varchar(25) DEFAULT NULL,
+  `kode_konsumen` varchar(7) DEFAULT NULL,
+  `plat_mobil` varchar(15) DEFAULT NULL,
+  `kerusakan` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_montir`
+--
+
+CREATE TABLE `tb_montir` (
+  `kode_montir` varchar(7) DEFAULT NULL,
+  `nama_montir` varchar(25) DEFAULT NULL,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `alamat` varchar(25) DEFAULT NULL,
+  `jenis_kelamin` varchar(10) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_montir`
+--
+
+INSERT INTO `tb_montir` (`kode_montir`, `nama_montir`, `no_hp`, `alamat`, `jenis_kelamin`) VALUES
+('M007', 'Manarul Hiadayat', '085367891236', 'Saturnus', 'Laki-Laki'),
+('M067', 'Arya Daulat', '085712345678', 'Pluto', 'Laki-Laki'),
+('M069', 'Fali Al-Farisi', '081398763425', 'Mars', 'Laki-Laki'),
+('M076', 'Muhammad Nasrullah', '081234567890', 'Jupiter', 'Laki-Laki');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
